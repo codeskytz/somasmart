1156ec49e0c13212316d17016898c5be938a944f
somasmartai <somasmartai@gmail.com>
2025-12-02 14:07:39 +0000

    ix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+
+import React, { useState } from 'react';
 import logo from './somasmart.png';
 import './App.css';
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <div className="header-content">
+          <button className="sidebar-toggle" onClick={toggleSidebar}>
+            &#9776;
+          </button>
+          <h1 className="header-title">SOMASMARTAI</h1>
+        </div>
+      </header>
+      <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Contact</a>
+      </div>
+      <div className="main-content">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Soon on release
@@ -17,7 +39,7 @@ function App() {
         >
          BROUGHT BY @CODESKYTZ
         </a>
-      </header>
+      </div>
     </div>
   );
 }
